25b9fced5c5cda6bcbb1cdd85045838a9350508c
Duong Tuan <tuanduong1209@gmail.com>
2025-03-11 02:15:01 +0700

    update code

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,20 +3,16 @@ import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
-
 export async function POST(req: Request) {
   const svix_id = headers().get("svix-id") ?? "";
   const svix_timestamp = headers().get("svix-timestamp") ?? "";
   const svix_signature = headers().get("svix-signature") ?? "";
-
   if (!process.env.WEBHOOK_SECRET) {
     throw new Error("WEBHOOK_SECRET is not set");
   }
-
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response("Bad Request", { status: 400 });
   }
-
   const payload = await req.json();
   const body = JSON.stringify(payload);
 
@@ -30,14 +26,14 @@ export async function POST(req: Request) {
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
     }) as WebhookEvent;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (err) {
     return new Response("Bad Request", { status: 400 });
   }
 
   const eventType = msg.type;
-
   if (eventType === "user.created") {
+    // create user to database
     const { id, username, email_addresses, image_url } = msg.data;
     const user = await createUser({
       username: username!,
@@ -51,8 +47,6 @@ export async function POST(req: Request) {
       user,
     });
   }
-
-  // Rest
 
   return new Response("OK", { status: 200 });
 }
